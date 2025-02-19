581f24669e2c7299094a8bdbfee49e8a20b98948
gonjila <lgonjila@gmail.com>
2025-02-20 00:03:16 +0400

    initial commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+
 import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -10,7 +11,48 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript"],
+    rules: {
+      "no-console": 1,
+      "@typescript-eslint/no-unused-vars": 1,
+      "@typescript-eslint/no-explicit-any": 1,
+      "import/extensions": 0,
+      "import/no-duplicates": 2,
+      "import/no-useless-path-segments": 1,
+      "import/prefer-default-export": "off",
+      "import/order": [
+        "warn",
+        {
+          "newlines-between": "always",
+          groups: ["builtin", "external", "internal", "parent", "sibling", "index"],
+        },
+      ],
+      "import/no-extraneous-dependencies": [
+        "error",
+        {
+          devDependencies: true,
+          optionalDependencies: false,
+          peerDependencies: false,
+        },
+      ],
+    },
+    settings: {
+      "import/resolver": {
+        typescript: {
+          alwaysTryTypes: true,
+          project: "./tsconfig.json",
+          extensions: [".ts", ".tsx"],
+          paths: ["./src"],
+          baseUrl: "./",
+          tsconfigRootDir: "./",
+        },
+        node: {
+          paths: ["./src"],
+        },
+      },
+    },
+  }),
 ];
 
 export default eslintConfig;
